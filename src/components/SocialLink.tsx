b3a90091531bbe5d11962911a251c5564fd22e7a
Yamahar <c19yamamoto@gmail.com>
2025-04-06 17:17:17 +0900

    サイト構築中ページ の追加 (#4)
    * SNS の追加
    
    * :sparkles:feat: update
    
    * :recycle:refactor: リファクタ
    
    * :recycle:refactor: リファクタ

---
+++ src/components/SocialLink.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './SocialLink.module.scss';
+
+type SocialLinkProps = {
+  href: string;
+  label: string;
+  children: React.ReactNode;
+};
+
+const SocialLink: React.FC<SocialLinkProps> = ({ href, label, children }) => {
+  return (
+    <a 
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles.socialLink}
+      aria-label={label}
+    >
+      {children}
+    </a>
+  );
+};
+
+export default SocialLink;
